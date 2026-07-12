01cb524c145f3a587e78dffb9dba358f78b2bced
Pearl Almeida <167133795+pearlalmeida17@users.noreply.github.com>
2026-07-12 16:45:34 -0700

    test cors wildcard

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://sphere-pearalmeida.vercel.app"],
+    allow_origins=["*"],
+    #allow_origins=["http://localhost:5173", "https://sphere-pearalmeida.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
